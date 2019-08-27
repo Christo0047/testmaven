7bb1f259af64702b26d5eed90f15a4b8e7aff3ba
dinesahu <dinesahu@AMGENEXTWEB.COM>
2019-08-27 19:11:34 +0530

    maven project

---
+++ src/com/app/service/HelloHTML.java
@@ -0,0 +1,12 @@
+package com.app.service;
+
+public class HelloHTML {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Index of mine first Application");
+		
+	}
+
+}
